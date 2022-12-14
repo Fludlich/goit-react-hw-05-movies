b42514a8ae86eb341eb7d4424e0d38126224b5be
Yurii Vynar <yurik.vinar37@gmail.com>
2022-12-14 11:16:34 +0200

    add button "

--- src/Pages/MovieDetails/MovieDetails.jsx
+++ src/Pages/MovieDetails/MovieDetails.jsx
@@ -1,7 +1,9 @@
+import { useNavigate } from 'react-router-dom';
 import { Link, Outlet, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { getMovieById } from '../../Services/services';
-import imgDefault from '../../defaultImg/imgDefault.png'
+import imgDefault from '../../defaultImg/imgDefault.png';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
 
 export function MovieDetails() {
   const { movieId } = useParams();
@@ -18,38 +20,70 @@ export function MovieDetails() {
       .catch(error => console.log(error));
   }, [movieId]);
 
+  const navigate = useNavigate();
+
   return (
     <div>
+      <Link to={`/movies`}>
+        {' '}
+        <button
+          onClick={() => navigate(-1)}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            width: '90px',
+            padding: '3px 8px',
+          }}
+        >
+          <AiOutlineArrowLeft /> Go back
+        </button>
+      </Link>
+
       {movie &&
-        movie.map(({id, title, poster_path, release_date, vote_average,overview,genres}) => {
-          return (
-            <div key={id}>
-              <img
-                src={poster_path? `https://image.tmdb.org/t/p/w500${poster_path}` : imgDefault }
-                alt={`${title}`}
-                width="300"
-              />
-              <h2>
-                {title}
-                {release_date}
-              </h2>
-              <p>User score: {vote_average}</p>
-              <h3>Overviev</h3>
-              <p>{overview}</p>
-              <h4>Ganres</h4>
-              <p>
-                {genres.map(el => {
-                  return el.name;
-                })}
-              </p>
-            </div>
-          );
-        })}
+        movie.map(
+          ({
+            id,
+            title,
+            poster_path,
+            release_date,
+            vote_average,
+            overview,
+            genres,
+          }) => {
+            return (
+              <div key={id}>
+                <img
+                  src={
+                    poster_path
+                      ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                      : imgDefault
+                  }
+                  alt={`${title}`}
+                  width="300"
+                />
+                <h2>
+                  {title}
+                  {release_date}
+                </h2>
+                <p>User score: {vote_average}</p>
+                <h3>Overviev</h3>
+                <p>{overview}</p>
+                <h4>Ganres</h4>
+                <p>
+                  {genres.map(el => {
+                    return el.name;
+                  })}
+                </p>
+              </div>
+            );
+          }
+        )}
       <p>Aditional information</p>
       <ul>
         {navItem.map(({ href, text }) => {
           return (
-            <Link to={href} key={href}>
+            <Link to={href} key={href} replace>
               {text}
             </Link>
           );
--- src/Pages/Movies/Movies.jsx
+++ src/Pages/Movies/Movies.jsx
@@ -1,27 +1,28 @@
 import { Link, Outlet } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { getMovieByName } from '../../Services/services';
-import { useSearchParams } from "react-router-dom";
-import imgDefault from '../../defaultImg/imgDefault.png'
+import { useSearchParams } from 'react-router-dom';
+import imgDefault from '../../defaultImg/imgDefault.png';
 
 export function Movies() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [inputValue, setInputValue] = useState('');
   const [params, setParams] = useState('');
   const [searchedMovie, setSearchedMovie] = useState([]);
-  const filmName = searchParams.get("query") ?? "";
 
+  const filmName = searchParams.get('query') ?? '';
 
-  const updateQueryString = (query) => {
-    const nextParams = query !== "" ? { query } : {};
+  const updateQueryString = query => {
+    const nextParams = query !== '' ? { query } : {};
     setSearchParams(nextParams);
   };
 
+  useEffect(() => {
+    if (filmName.length > 2 && params.length < 1) {
+      setInputValue(filmName);
+    }
+  }, [filmName, params.length]);
 
-  if(filmName.length>2 && params.length<1){
-    setParams(filmName)
-    setInputValue(filmName)
-  }
   useEffect(() => {
     if (params.length > 2) {
       getMovieByName(params)
@@ -32,17 +33,13 @@ export function Movies() {
     }
   }, [params]);
 
-
-
   const handlerForm = e => {
     e.preventDefault();
-    
-    // setSearchParams({ name: inputValue })
     setParams(inputValue.trim().toLowerCase());
   };
   const handlerInput = event => {
     setInputValue(event.target.value);
-    updateQueryString(event.target.value)
+    updateQueryString(event.target.value);
   };
 
   return (
@@ -54,12 +51,16 @@ export function Movies() {
       {searchedMovie.length > 0 &&
         (searchedMovie.length > 1 ? (
           <ul style={{ display: 'flex', flexWrap: 'wrap', gap: '30px' }}>
-            {searchedMovie.map(({id, original_title, poster_path, title}) => {
+            {searchedMovie.map(({ id, original_title, poster_path, title }) => {
               return (
                 <Link to={`${id}`} key={id}>
                   <div>
                     <img
-                      src={poster_path? `https://image.tmdb.org/t/p/w500${poster_path}` : imgDefault}
+                      src={
+                        poster_path
+                          ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                          : imgDefault
+                      }
                       alt={`${title}`}
                       width="300"
                     />
@@ -72,7 +73,11 @@ export function Movies() {
         ) : (
           <Link to={`${searchedMovie[0].id}`}>
             <img
-              src={searchedMovie[0].poster_path? `https://image.tmdb.org/t/p/w500${searchedMovie[0].poster_path}` : imgDefault}
+              src={
+                searchedMovie[0].poster_path
+                  ? `https://image.tmdb.org/t/p/w500${searchedMovie[0].poster_path}`
+                  : imgDefault
+              }
               alt={`${searchedMovie[0].title}`}
               width="300"
             />
@@ -84,4 +89,3 @@ export function Movies() {
     </div>
   );
 }
-
